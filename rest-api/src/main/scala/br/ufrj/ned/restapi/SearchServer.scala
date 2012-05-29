14bd4197e77245d1b6967602d4cd0e974582f439
Arthur Bit-Monnot <arthur.bitmonnot@gmail.com>
2012-05-29 14:46:47 -0300

    Make the web service a bit cleaner

---
+++ rest-api/src/main/scala/br/ufrj/ned/restapi/SearchServer.scala
@@ -0,0 +1,49 @@
+package br.ufrj.ned.restapi
+
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+
+import javax.ws.rs.core.UriBuilder;
+
+
+object SearchServer {
+
+    private def getPort(defaultPort : Int) = {
+        val port = System.getProperty("jersey.test.port");;
+
+        if (null != port)
+            try  {
+              Integer.parseInt(port);
+            } catch {
+                case ex: NumberFormatException => defaultPort;
+            }
+        else
+            defaultPort;
+    }
+
+    private def getBaseURI() = {
+        UriBuilder.fromUri("http://localhost/").port(getPort(9998)).
+            path("ned").build();
+    }
+
+    val BASE_URI = getBaseURI();
+
+    def startServer() = {
+        val rc = new PackagesResourceConfig("br.ufrj.ned.restapi");
+
+        println("Starting grizzly...");
+        GrizzlyServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    def main(args: Array[String]) {
+        val httpServer = startServer();
+
+        println("Server running");
+        println("Hit return to stop...");
+        System.in.read();
+        println("Stopping server");
+        httpServer.stop();
+        println("Server stopped");
+        System.exit(0);
+    }
+}
